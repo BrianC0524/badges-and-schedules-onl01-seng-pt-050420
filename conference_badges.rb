12e558467a4ee3f17c828f928d16c8a4c614d38a
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-14 04:39:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,33 +10,20 @@ end
 
 def batch_badge_creator(names)
   
-  badge_message = []
-  
-  names.collect {|name| badge_message << badge_maker(name)}
-  
-  return badge_message
+  names.map {|name| "Hello, my name is #{name}."}
   
 end
 
 def assign_rooms(names)
   
-  assigned_rooms = []
-  
-  names.each_with_index {|name, index| assigned_rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
-  
-  return assigned_rooms
+  names.each_with_index.map {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
   
 end
 
 def printer(names)
+
+  batch_badge_creator(names).each {|badge| puts badge}
   
-  badge_print = []
-  room_print =[]
-  
-  batch_badge_creator(names).collect {|message| badge_print << message}
-  assign_rooms(names).collect {|message| room_print << message}
-  
-  badge_print.each {|message| puts message}
-  room_print.each {|message| puts message}
+  assign_rooms(names).each {|assign| puts assign}
   
 end
